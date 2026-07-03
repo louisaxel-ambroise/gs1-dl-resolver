b298eb191f7f1a898aa4040d0b3299c68f4d4395
Louis-Axel Ambroise <ambroise.la@pm.me>
2026-07-03 17:29:46 +0200

    Cleanup

--- Model/EPCs/DefinitionFile.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Text.Json.Serialization;
-
-namespace FasTnT.TagDataTranslation.Model.EPCs;
-
-public class DefinitionFile
-{
-    [JsonPropertyName("tdt:epcTagDataTranslation")]
-    public EpcTagDataTranslation TagDataTranslation { get; set; }
-}
